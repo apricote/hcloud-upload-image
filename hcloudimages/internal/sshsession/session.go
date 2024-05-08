06284185c8db3506d3e52dcf9d3edf0ea5688f07
Julian Tölle <julian.toelle97@gmail.com>
2024-05-08 23:36:33 +0200

    feat: upload local disk images
    The new options/flag enables users to use a local file as the image,
    instead of a publicly available file from a web server.

--- hcloudimages/internal/sshsession/session.go
+++ hcloudimages/internal/sshsession/session.go
@@ -1,12 +1,21 @@
 package sshsession
 
-import "golang.org/x/crypto/ssh"
+import (
+	"io"
 
-func Run(client *ssh.Client, cmd string) ([]byte, error) {
+	"golang.org/x/crypto/ssh"
+)
+
+func Run(client *ssh.Client, cmd string, stdin io.Reader) ([]byte, error) {
 	sess, err := client.NewSession()
+
 	if err != nil {
 		return nil, err
 	}
 	defer sess.Close()
+
+	if stdin != nil {
+		sess.Stdin = stdin
+	}
 	return sess.CombinedOutput(cmd)
 }
